50a66fb46de3e1b60a333d38e2e776de0008f5ee
chindo <Lavenderwinnifred@gmail.com>
2023-07-13 12:37:00 +0100

    hey

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def days_to_units(x, custom_message):
     print(f"{x} days has {x * calculate_seconds} seconds")
     print(custom_message)
 
-def
+def lfrrgj():
 
-days_to_units(70, "yess!")
+days_to_units(70, "yass!")
 days_to_units(110, "great!")
 days_to_units(150, "we doing it")
 days_to_units(190, "All good, Peace")
