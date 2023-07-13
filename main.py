e410511349aae43ff04329fa5d19f5ba8ab322e6
chindo <Lavenderwinnifred@gmail.com>
2023-07-13 12:04:55 +0100

    learnings

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print ("HELLO WORLD")
+
+calculate_seconds = 24 * 60 * 60
+
+
+def days_to_units(x, custom_message):
+    print(f"{x} days has {x * calculate_seconds} seconds")
+    print(custom_message)
+
+def
+
+days_to_units(70, "yess!")
+days_to_units(110, "great!")
+days_to_units(150, "we doing it")
+days_to_units(190, "All good, Peace")
