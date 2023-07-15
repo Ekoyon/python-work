12ce8024eae6de90a16758f4b8770452d7297e7a
Ekoyon <104011843+Ekoyon@users.noreply.github.com>
2023-07-15 12:25:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("HELLO WORLD")
+
+calculate_seconds = 24 * 60 * 60
+
+
+def days_to_units(x, custom message):
+    print(f"{x} days has {x * calculate_seconds} seconds")
+    print(custom_message)
